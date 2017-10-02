22c42e9714a169d037dc47fddffc36cedd54df53
Pawel <pawel.html@gmail.com>
2017-10-02 22:45:49 +0200

    settings basic

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import addReminder from '../actions'
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+
+    }
+
+
+    addReminder(){
+        console.log("this.props from addReminder: ", this.props);
+    }
+
+
+    render(){
+        return(
+            <div className="App">
+                <div className="app_title">Title</div>
+                <form>
+                    <input 
+                        placeholder="Thing to do..."
+                        onChange={event=>{
+                                this.setState({
+                                    text: event.target.value
+                                });
+                        }}
+                        value={this.state.text}
+                        onKeyPress={e=>{
+                            if(e.key === "Enter") {
+                                e.preventDefault();
+                                this.addReminder()
+                                };
+                        }}
+                    />
+                    <button 
+                        type="button"
+                        onClick={()=>this.addReminder()}
+                    >
+                        Add reminder
+                    </button>
+                </form>
+            </div>
+        );
+    };
+};
+
+export default App;
